a43550fad63fa006b0835c8354f33ece6711b672
SewwandiPeiris <peirissewwandi@gmail.com>
2024-06-05 23:57:36 +0530

    component html and css

--- Day12/demo-app/src/app/app.component.ts
+++ Day12/demo-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./pages/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { SuccessComponent } from "./pages/success/success.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, RegisterComponent, SuccessComponent]
 })
 export class AppComponent {
   title = 'demo-app';
--- Day12/demo-app/src/app/app.routes.ts
+++ Day12/demo-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { SuccessComponent } from './pages/success/success.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+    {
+        path:'success',
+        component:SuccessComponent
+    }
+];
